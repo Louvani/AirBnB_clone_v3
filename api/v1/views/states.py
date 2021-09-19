bcfc3306034e56ebc0e901f4aa23bb4bf9662429
Louvani <paula.louvani@gmail.com>
2021-09-19 16:34:10 -0500

    Return dict for states and specific state

---
+++ api/v1/views/states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""States"""
+
+from api.v1.views import app_views
+from flask import Flask, jsonify, redirect
+from models import storage
+from models.state import State
+from api.v1.app import not_found
+
+
+@app_views.route('/states', strict_slashes=False, methods=['GET'])
+def states():
+    # objects dictionary
+    states_dict = storage.all(State)
+    #  Iterate -> to dict -> save in list
+    states_list = [state.to_dict() for state in states_dict.values()]
+    print(states_list)
+    return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
+def state_by_id(state_id):
+    state = storage.get(State, state_id)
+    if state is not None:
+        return jsonify(state.to_dict())
+    return jsonify({"error": "Not found"}), 404
