973e4c4b39b65f1a5a4ee6951c16f36a11eccb60
Vanessa <vagava.123@gmail.com>
2021-09-19 22:02:44 -0500

    fix pep 8 in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,14 +13,15 @@ from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-	return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-	return jsonify(
-		{"amenities": storage.count(Amenity),
-		"cities": storage.count(City),
-		"places": storage.count(Place),
-		"reviews": storage.count(Review),
-		"states": storage.count(State),
-		"users": storage.count(User)})
+    return jsonify(
+        {"amenities": storage.count(Amenity),
+         "cities": storage.count(City),
+         "places": storage.count(Place),
+         "reviews": storage.count(Review),
+         "states": storage.count(State),
+         "users": storage.count(User)})
