13a931b0b4f1f721fa58c98a94ccb78804f9dd28
Vanessa <vagava.123@gmail.com>
2021-09-19 22:25:03 -0500

    move app.teardown_appcontext to line 20

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,14 +12,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
-
-
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({"error": "Not found"}), 404
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
 
 
 if __name__ == '__main__':
